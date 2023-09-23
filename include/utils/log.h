89b82768a1aa2f84d3a31649465b0c676a8abf51
nomi <kimon.kukulis@gmail.com>
2023-09-23 13:07:10 +0300

    handy log macro

--- include/utils/log.h
+++ include/utils/log.h
@@ -1,6 +1,29 @@
 #ifndef UTILS_LOG_H
 #define UTILS_LOG_H
 
+#include <klib/stdio.h>
+
+
+
+#ifdef DEBUG
+
+char *getFileName(const char *path) {
+    const char *filename = path;
+    const char *ptr = path;
+
+    while (*ptr) {
+        if (*ptr == '/' || *ptr == '\\') {
+            filename = ptr + 1;
+        }
+        ptr++;
+    }
+    return (char *)filename;
+}
+
+#define klog(fmt, ...) kprintf("%u:%s:%s-> " fmt, __LINE__, getFileName(__FILE__), __FUNCTION__, ##__VA_ARGS__)
+#else
+#define klog(fmt, ...) kprintf(fmt, ##__VA_ARGS__)
+#endif
 
 extern void log(char *str);
 
